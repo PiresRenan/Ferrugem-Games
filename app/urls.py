a7b8fd640b0332ff381bb11682dcb6c8759cc9fe
Pirasampa <jvsampa15@gmail.com>
2022-11-20 12:33:10 -0300

    quase final

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from .views import index, games, cadastro, shopping
 
 urlpatterns = [
     path('', index, name='index'),
-    path('games', games, name='games'),
+    path('games', games, name='game'),
     path('cadastro', cadastro, name='cadastro'),
     path('shopping', shopping, name='shopping') 
 ]
